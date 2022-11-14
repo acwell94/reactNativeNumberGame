82a54294437261c5c0070822cc54aa7934633cc0
acwell94 <acwell94@naver.com>
2022-11-14 20:55:24 +0900

    버튼 피드백 개선, 버튼 로직 구성, input 유효성 검사

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import { Alert, StyleSheet, TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  const [enteredNumber, setEnteredNumber] = useState("");
+
+  function numberInputHandler(enteredText) {
+    setEnteredNumber(enteredText);
+  }
+  function resetInputHandler() {
+    setEnteredNumber("");
+  }
+  function confirmInputHandler() {
+    console.log(enteredNumber);
+
+    if (
+      isNaN(enteredNumber) ||
+      Number(enteredNumber) <= 0 ||
+      Number(enteredNumber) > 99
+    ) {
+      Alert.alert("invalid Number", "please write 1~99 Number", [
+        {
+          text: "Okay!",
+          style: "destructive",
+          onPress: resetInputHandler,
+        },
+        ,
+      ]);
+    }
+    console.log("valid Number");
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={numberInputHandler}
+        value={enteredNumber}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
